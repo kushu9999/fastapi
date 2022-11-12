e667da367bafa24eff05a6a4e219742d960f7e1e
kushu9999 <kushu9999@gmail.com>
2022-11-12 10:22:23 +0530

    refactored the code

--- services/api/main.py
+++ services/api/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from router import blog_get, blog_get_with_prefix, blog_post
+from services.api.router import blog_get, blog_get_with_prefix, blog_post
 
 app = FastAPI()
 app.include_router(blog_get.router)
 app.include_router(blog_get_with_prefix.router)
 app.include_router(blog_post.router)
 
-@app.get("/hello")
+@app.get("/")
 def index():
     return {"message": "Hello World"}
 
